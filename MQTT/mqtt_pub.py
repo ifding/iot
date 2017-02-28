a61a70579c223fbcdee942c003bfd078b2767e08
feiware <fding87@hotmail.com>
2017-02-27 20:07:17 -0500

    Update mqtt_pub.py

--- MQTT/mqtt_pub.py
+++ MQTT/mqtt_pub.py
@@ -1,3 +1,6 @@
+import sys
+import datetime
+import time
 import paho.mqtt.client as mqtt
 
 client=mqtt.Client()
@@ -5,5 +8,10 @@ client=mqtt.Client()
 # mqtt broker
 client.connect('iot.eclipse.org',1883)
 
+# get the current timestamp
+timestamp =datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S)
+
+payload = "{\"device_id\": 001, \"timestamp\": " + str(timestamp) + ", \"message\": hellow world }"                                    
+                                      
 # topic, message 
-client.publish('iot/tiger','Go Tigers!')
+client.publish('iot/tiger', payload)
